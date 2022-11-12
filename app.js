4baa443f6a210656a2e5f1258d661a7443f88d1e
Justin Doyle <justin@jmdoyle.com>
2022-11-12 04:06:39 -0600

    Added players and characters routers

--- app.js
+++ app.js
@@ -7,15 +7,27 @@ const port = 8080;
 app.use(express.json());
 app.use(express.static("express"));
 
-var createError = require("http-errors");
+let createError = require("http-errors");
+
+let mysql      = require('mysql2');
+global.connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'jdoyle',
+    password : 'varos',
+    database : 'chron_manager'
+});
 
 
 // ----- ROUTES ----- //
 //var indexRouter = require("./public/routes/index");
+const playersRouter = require('./routes/players.js');
+app.use("/players", playersRouter);
 
+const charactersRouter = require('./routes/characters');
+app.use("/characters", charactersRouter);
 
 // ----- DATABASE ----- //
-
+global.connection.connect();
 
 
 // ----- VIEWS ----- //
@@ -28,8 +40,26 @@ app.use("/", indexRouter);
 
  */
 
+/*
+ * Log all incoming requests
+ */
+app.use(function (req, res, next) {
+    console.log(`[${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}] ${req.ip} ${req.method} ${req.path}`);
+    return next();
+});
+
+
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}.  localhost:${port}`);
+
+    global.connection.query('SELECT * FROM cms_players', function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+    });
 });
+
+
+
 
 module.exports = app;
--- schemas/create-tables.sql
+++ schemas/create-tables.sql
@@ -12,13 +12,18 @@ CREATE TABLE IF NOT EXISTS cms_players
     firstName        VARCHAR(20),
     lastName         VARCHAR(20),
     waiverDocumentId VARCHAR(40),
-    cocDocumentId    VARCHAR(40)
+    cocDocumentId    VARCHAR(40),
+
+    UNIQUE(email)
 );
 
 INSERT IGNORE INTO cms_players (playerId, email, password, staffPermissions, plotPermissions, firstName, lastName)
 VALUES ("00000000-0000-0000-0000-000000000001", "justin@jmdoyle.com", "changeMe2!", 3, 3, "Justin", "Doyle");
 
 
+/**
+  cms_documents is for storing things like signed Code of Conduct and Liability Waiver forms
+ */
 CREATE TABLE IF NOT EXISTS cms_documents
 (
     documentId   VARCHAR(40) DEFAULT (uuid()) PRIMARY KEY,
@@ -36,7 +41,7 @@ CREATE TABLE IF NOT EXISTS cms_races
 (
     raceId       VARCHAR(40) DEFAULT (uuid()) PRIMARY KEY,
     name         varchar(40),
-    parentRaceId VARCHAR(40),
+    parentRaceId VARCHAR(40), /* Races such as Elves can have sub-races, ie Wild Elves, Sun Elves, etc.. */
     FOREIGN KEY (parentRaceId) REFERENCES cms_races (raceId)
 );
 
@@ -49,7 +54,7 @@ CREATE TABLE IF NOT EXISTS cms_skills
     skillId          VARCHAR(40) DEFAULT (uuid()) PRIMARY KEY,
     name             VARCHAR(40),
     description      TEXT,
-    isSpSkill        BOOLEAN,
+    isSpSkill        BOOL,
     IsTransformSkill BOOL,
     isRacialSkill    BOOL,
     baseCostFighter  INT,
@@ -101,17 +106,17 @@ CREATE TABLE IF NOT EXISTS cms_characters
 (
     characterId      VARCHAR(40) DEFAULT (uuid()) PRIMARY KEY,
     playerId         VARCHAR(40),
-    dateCreated      DATETIME,
+    dateCreated      DATETIME DEFAULT(NOW()),
     name             VARCHAR(40),
     title            VARCHAR(40),
-    activeStatus     VARCHAR(10),
+    activeStatus     VARCHAR(10) DEFAULT("active"),
     raceId           VARCHAR(40),
-    subRace          VARCHAR(40),
-    skillPointsTotal INT,
-    skillPointsSpend INT,
+    subRaceId        VARCHAR(40),
+    skillPointsTotal INT DEFAULT(100),
+    skillPointsSpent INT DEFAULT(0),
     class            VARCHAR(10),
-    strain           INT,
-    bodyMax          INT,
+    strain           INT DEFAULT(0),
+    bodyMax          INT DEFAULT(5),
     armorMax         INT,
     transformId      VARCHAR(40),
 
@@ -120,8 +125,8 @@ CREATE TABLE IF NOT EXISTS cms_characters
     FOREIGN KEY (transformId) REFERENCES cms_transforms (transformId)
 );
 
-INSERT IGNORE INTO cms_characters (characterId, playerId, dateCreated, name, title, activeStatus, raceId, subRace,
-                                   skillPointsTotal, skillPointsSpend, class, strain, bodyMax, armorMax, transformId)
+INSERT IGNORE INTO cms_characters (characterId, playerId, dateCreated, name, title, activeStatus, raceId, subRaceId,
+                                   skillPointsTotal, skillPointsSpent, class, strain, bodyMax, armorMax, transformId)
 VALUES ("00000000-0000-0000-0000-00000000000Z", "00000000-0000-0000-0000-000000000001", NOW(), "Z", "Ambassador",
         "active", "00000000-0000-0000-0000-000000000003", "Spider", 700, 0, "Mage", 0, 10, 0,
         "00000000-0000-0000-0000-000000000004");
