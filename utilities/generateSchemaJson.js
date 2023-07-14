d538ca521b4c4a9eb3f1982919c7765c06cd00f3
Justin Doyle <justin@jmdoyle.com>
2023-07-13 22:01:34 -0500

    Added JSON schema generator file

---
+++ utilities/generateSchemaJson.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+const schemaDir = "schemas/";
+
+fs.readdir(schemaDir, function (err, files) {
+    let outputContent = {};
+
+    if (err) { return console.log('Unable to scan directory: ' + err); }
+    for (let i = 0; i < files.length; i++) {
+        let filename = files[i];
+        // Only match files that fit the format "entitySchema-something.json"
+        if (filename.search(/^entitySchema-.*\.json$/) > -1) {
+            let schema = require(path.join("..", schemaDir, filename));
+            let propName = filename.replace("entitySchema-", "").replace(".json", "");
+            outputContent[propName] = schema;
+        }
+    }
+    console.log(JSON.stringify(outputContent, null, 4));
+    
+});
